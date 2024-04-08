9024ec7e1dfa8986f4e8d78117c4085c0e7ca8cf
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-08 18:04:47 +0300

    refactor: update token if expiretion

--- src/main/java/api/pietunes/snoopy/utils/TokenContainer.java
+++ src/main/java/api/pietunes/snoopy/utils/TokenContainer.java
@@ -3,15 +3,40 @@ package api.pietunes.snoopy.utils;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import lombok.Data;
+
 public class TokenContainer {
-    private static final Map<String, String> tokenMap = new ConcurrentHashMap<>();
+
+    private final static long expirationTime = 3600;
+
+    @Data
+    static class Token {
+
+        public Token(String value) {
+            this.value = value;
+            this.createdAt = System.currentTimeMillis();
+        }
+
+        private String value;
+        private long createdAt;
+    }
+    
+    private static final Map<String, Token> tokenMap = new ConcurrentHashMap<>();
 
     public static String getToken(String identifier) {
-        return tokenMap.get(identifier);
+        Token token = tokenMap.get(identifier);
+        long currentTime = System.currentTimeMillis();
+        
+        if ((token.getCreatedAt() + expirationTime) <= currentTime) {
+            invalidateToken(identifier);
+            return null;
+        } else {
+            return token.getValue();
+        }
     }
 
     public static void saveToken(String identifier, String token) {
-        tokenMap.put(identifier, token);
+        tokenMap.put(identifier, new Token(token));
     }
 
     public static void invalidateToken(String identifier) {
